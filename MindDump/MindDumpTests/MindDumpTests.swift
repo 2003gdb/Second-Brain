782aa9bf66f3501f5118310bb0f92dadf4413d60
2003gdb <2003gdb@gmail.com>
2025-12-29 16:55:26 -0600

    first commit

---
+++ MindDump/MindDumpTests/MindDumpTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindDumpTests.swift
+//  MindDumpTests
+//
+//  Created by Carlos A on 15/12/25.
+//
+
+import Testing
+@testable import MindDump
+
+struct MindDumpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
